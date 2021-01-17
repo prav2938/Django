3cd209931746feb6aff2d4f66bca052697f0b1bd
Ravi Pandey <pravi2938@gmail.com>
2021-01-17 23:53:58 +0530

    final commit

--- resumeproject/edu/views.py
+++ resumeproject/edu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def skills(request):
+    return render(request,'edu/skills.html')
--- resumeproject/resumeproject/urls.py
+++ resumeproject/resumeproject/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path,include
 from core import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home , name = "home"),
-    path('serv/',include('serv.urls'))
+    path('serv/',include('serv.urls')),
+    path('edu/',include('edu.urls')),
+    path('contact/', views.contact , name = "contact"),
 ]
